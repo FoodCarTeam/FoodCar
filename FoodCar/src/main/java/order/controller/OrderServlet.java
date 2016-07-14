6198aac61b8c85bff46e26431412b61fe6e627f8
江宗翰 <@EEIT>
2016-07-14 14:33:11 +0800

    翰翰 新增遺失的Js檔，測試order的servlet

---
+++ FoodCar/src/main/java/order/controller/OrderServlet.java
@@ -0,0 +1,33 @@
+package order.controller;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/order")
+public class OrderServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+   
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setHeader("content-type", "text/html;charset=UTF-8");
+		response.setCharacterEncoding("UTF-8");
+		
+		String prices = request.getParameter("unitPrice");
+	
+		
+		
+			
+			System.out.println(prices);
+		
+	}
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {		
+		doGet(request, response);
+	}
+
+}
--- FoodCar/src/main/webapp/bootstrap/js/page.js
+++
@@ -1,7 +0,0 @@
-$(function () {
-    $("#MainContent .content .text img").wrap('<div class="image"></div>');
-
-    if (typeof mainImageCustomSign !== 'undefined') {
-        $("#MainContent div.image:first").append('<div class="custom-sign ' + mainImageCustomSign + '"></div>');
-    }
-});
