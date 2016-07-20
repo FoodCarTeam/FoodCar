1bc72f8d16b9a0824aee5dca7452f3f9ec77f8a4
翁榮亮 <johnsonweng0808@msn.com>
2016-07-20 15:30:25 +0800

    新增：storeLogin.servlet

---
+++ FoodCar/src/main/java/_19_storemaintain/model/StoresDAO.java
@@ -0,0 +1,20 @@
+package _19_storemaintain.model;
+
+import java.util.List;
+
+import model.StoresVO;
+
+public interface StoresDAO {
+	StoresVO select(Integer sID);
+	
+	StoresVO select2(String sUsername);
+
+	List<StoresVO> select();
+
+	StoresVO insert(StoresVO bean);
+
+	StoresVO update(StoresVO bean);
+
+	boolean delete(Integer sID);
+
+}
